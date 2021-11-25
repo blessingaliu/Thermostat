057b5a25b69e4f78d7f9e5b36ff9ee1206624007
Matt Zimmer <zimmermja@gmail.com>
2021-11-25 16:58:45 +0000

    began up function

--- thermostat.js
+++ thermostat.js
@@ -1,3 +1,11 @@
-class Thermostat {}
+class Thermostat {
+  constructor(temperature = 20) {
+    this.temperature = temperature;
+  }
+
+  getTemperature() {
+    return this.temperature;
+  }
+}
 
 module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,8 +1,15 @@
 const Thermostat = require("./thermostat");
 
 describe("Thermostat class", () => {
-  it("Is a test", () => {
-    // Test here
+  it("Create Thermostat class and get temperature", () => {
+    const thermostat = new Thermostat();
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+  it("Increase temperature using up method", () => {
+    const thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.up()).toEqual;
   });
 });
 
