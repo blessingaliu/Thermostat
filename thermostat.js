dbdc49e88c44273ecb0c9454baefd57304206eb3
Blessing Aliu <blessingaliu@ymail.com>
2021-11-25 17:25:44 +0000

    added a power saving mode and passed feature test

--- thermostat.js
+++ thermostat.js
@@ -1,6 +1,11 @@
 class Thermostat {
   constructor(temperature = 20) {
     this.temperature = temperature;
+    this.powerSavingMode = true
+  }
+  maxTemp() {
+    if (this.powerSavingMode === true) return 25;
+    return 32
   }
 
   getTemperature() {
@@ -8,7 +13,7 @@ class Thermostat {
   }
 
   up() {
-    return this.temperature += 1
+    if (this.temperature < this.maxTemp()) this.temperature += 1
   }
 
   down() {
--- thermostat.test.js
+++ thermostat.test.js
@@ -19,12 +19,19 @@ describe("Thermostat class", () => {
     thermostat.down();
     expect(thermostat.getTemperature()).toEqual(18);
   });
+
+  it("Does not increase over 25 when power saving is active", () => {
+    const thermostat = new Thermostat();
+    for (i = 0; i<50; i++) {
+      thermostat.up()
+    }
+    expect(thermostat.getTemperature()).toEqual(25);
+  })
 });
 
-// const thermostat = new Thermostat();
+// thermostat.setPowerSavingMode(true); // PSM is now on, 
+// max temperature is 25
 
-// thermostat.getTemperature(); // should return 20
-
-// thermostat.up();
-// thermostat.up();
-// thermostat.getTemperature(); // should now return 22
+// for (let i = 0 ; i < 10 ; i++) {
+//   thermostat.up();
+// }
